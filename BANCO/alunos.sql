2ff9e240304bba65141a720e41d42a70f793eef1
nuneszk <110551255+nuneszk@users.noreply.github.com>
2023-03-16 09:40:13 -0300

    Add files via upload

---
+++ BANCO/alunos.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 16-Mar-2023 às 13:38
+-- Versão do servidor: 10.4.24-MariaDB
+-- versão do PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `wendell`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `alunos`
+--
+
+CREATE TABLE `alunos` (
+  `ID` int(11) DEFAULT NULL,
+  `Nome` varchar(255) DEFAULT NULL,
+  `Sobrenome` varchar(255) DEFAULT NULL,
+  `Turma` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `alunos`
+--
+
+INSERT INTO `alunos` (`ID`, `Nome`, `Sobrenome`, `Turma`) VALUES
+(1, 'Wendell', 'Nunes', '3ºB'),
+(2, 'gabriel', 'martins', '3ºB'),
+(NULL, 'wendell', 'nunes', '3ºB');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ BANCO/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test</title>
+</head>
+<body>
+    
+<h2>Formulário</h2>
+
+
+
+<form action="index.php" method="post">
+  <label>Nome:</label><br>
+  <input type="text" id="n" name="n"><br><br>
+  <label>Sobrenome:</label><br>
+  <input type="text" id="s" name="s"><br><br>
+  <label>Turma:</label><br>
+  <input type="text" id="t" name="t"><br><br>
+  
+  <input type="submit" value="Enviar">
+</form>
+
+<?php 
+$nome =$_POST["n"];
+$sobrenome =$_POST["s"];
+$turma =$_POST["t"];
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "wendell";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "INSERT INTO alunos (Nome, Sobrenome, Turma)
+VALUES ('$nome', '$sobrenome', '$turma' )";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Novo cadastro feito com êxito";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+
+
+
+
+    <script src="script.js"></script>
+</body>
+</html>
