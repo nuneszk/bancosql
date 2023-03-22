4c8e161a0b8915b5381db85884f71da4cb77fd13
nuneszk <110551255+nuneszk@users.noreply.github.com>
2023-03-22 09:56:40 -0300

    s6

--- BANCO/agenda.php
+++ BANCO/agenda.php
@@ -1,3 +1,20 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP test</title>
+</head>
+<body>
+
+
+
+
+
+
 <?php 
 $nome =$_POST["n"];
 $sobrenome =$_POST["s"];
@@ -32,5 +49,6 @@ $conn->close();
 
 
 
-
+</body>
+</html>
     
